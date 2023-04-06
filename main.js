d1972987308ff730e3e1bdb91c19878d70f6b9cf
Ariel Lin <al2248@cornell.edu>
2023-04-05 21:04:32 -0400

    extension add works

---
+++ main.js
@@ -0,0 +1,7 @@
+const body = document.querySelector('body');
+const gif = document.createElement('img')
+gif.src = "https://s3.getstickerpack.com/storage/uploads/sticker-pack/twitch-pepe-meme-gif/sticker_17.gif?26a74f4f0ef5a787894adc441abd042e";
+body.appendChild(gif);
+gif.style.position = 'absolute';
+gif.style.top = '30%';
+gif.style.left = '30%';
