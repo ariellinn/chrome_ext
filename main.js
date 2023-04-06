dd7e6d7ac70248b3d190d1bfa0656cf095785509
Ariel Lin <ariellin@Fannys-iMac.fios-router.home>
2023-04-06 01:52:31 -0400

    replaced section content paragraphs with prgming memes

--- main.js
+++ main.js
@@ -4,14 +4,22 @@ const images = ["https://s3.getstickerpack.com/storage/uploads/sticker-pack/twit
   "https://media.tenor.com/JK3PUlvXIt8AAAAM/math-lady.gif",
   "https://media.tenor.com/HnJpjRirG5UAAAAC/jackie-chan-meme.gif",
   "https://media.tenor.com/9wHj0SnxEb8AAAAC/lost-confused.gif"
-]
+];
 
-const memes = []
+const memes = ["https://res.cloudinary.com/practicaldev/image/fetch/s--JcyEpOBL--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/au3f7tzbla8hydecno6q.jpg",
+"https://res.cloudinary.com/practicaldev/image/fetch/s--nJjfteXn--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_66%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/yyvpwe28d4cqv76rjjpz.gif",
+"https://res.cloudinary.com/practicaldev/image/fetch/s--oUchtkYq--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_66%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ade5cd9bl7wz7bnsqbkn.gif",
+"https://res.cloudinary.com/practicaldev/image/fetch/s--pwQ9FFbx--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/6a413tyi7awhnisccl62.jpg",
+"https://res.cloudinary.com/practicaldev/image/fetch/s--CU-rIdFU--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bq3roea252k0h783ugeo.png",
+"https://ih1.redbubble.net/image.3046220789.1503/flat,750x,075,f-pad,750x1000,f8f8f8.jpg",
+"https://testbytes.technoallianceindia.com/wp-content/uploads/2019/06/Untitled-8-1.png",
+"https://assets-global.website-files.com/5f3c19f18169b62a0d0bf387/60d33be8cf4ba7565123c8bc_YPD3ulQQAGQpOcnqIm3QzSTRgzmr1SexpW9ZjMpJ1mAnUxx4iF05XOTu44sk0qQG-8XgBcYmGZGAD-5SAZvJl3TjtmhgWnn-w0C2XKwhBscV78RVvhwZfyp0v_Pa6sNj5zxpOvRW.png"];
 
+const usedMemes = new Set();
 
 const body = document.querySelector('body');
 const gif = document.createElement('img');
-gif.src = images[Math.floor(Math.random() * 6)];
+gif.src = images[Math.floor(Math.random() * images.length)];
 body.appendChild(gif);
 gif.style.position = 'fixed';
 gif.style.width = "10%"
@@ -34,5 +42,30 @@ function replaceEveryThreeLinks() {
   }
 }
 
+function replaceParagraph() {
+  const divParagraphs = document.querySelectorAll("div.section-content > p");
+  if (divParagraphs.length !== 0) {
+    let randomMeme = Math.floor(Math.random() * memes.length);
+    let randomP = Math.floor(Math.random() * divParagraphs.length);
+    const newImage = document.createElement('img');
+    if (usedMemes.size !== memes.length) {
+      do {
+        randomMeme = Math.floor(Math.random() * memes.length);
+      } while (usedMemes.has(randomMeme))
+    }
+    newImage.src = memes[randomMeme];
+    newImage.style.width = "50%";
+    usedMemes.add(randomMeme);
+    divParagraphs[randomP].parentNode.replaceChild(newImage, divParagraphs[randomP]);
+
+    setTimeout(replaceParagraph.bind(this), 5000);
+    console.log("calling replaceParagrph");
+  }
+
+
+
+}
+
 increment();
 replaceEveryThreeLinks();
+setTimeout(replaceParagraph, 5000);
